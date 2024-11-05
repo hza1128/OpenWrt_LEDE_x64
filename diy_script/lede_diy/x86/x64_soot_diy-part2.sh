25429ff36eec2465897d453824b6ab652f61d5a9
3092099 <122336705+3092099@users.noreply.github.com>
2024-11-06 00:29:32 +0800

    Update x64_soot_diy-part2.sh

--- diy_script/lede_diy/x86/x64_soot_diy-part2.sh
+++ diy_script/lede_diy/x86/x64_soot_diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #===============================================
 # Description: DIY script part 2
-# File name: diy-part2.sh
+# File name: x64_soot_diy-part2.sh
 # Lisence: MIT
 # By: GXNAS
 #===============================================
