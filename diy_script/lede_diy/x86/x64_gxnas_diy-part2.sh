cab6659419d1b9a9bf0b3a03f75ecfc98a5d4566
3092099 <122336705+3092099@users.noreply.github.com>
2024-11-06 22:38:05 +0800

    Update x64_gxnas_diy-part2.sh

--- diy_script/lede_diy/x86/x64_gxnas_diy-part2.sh
+++ diy_script/lede_diy/x86/x64_gxnas_diy-part2.sh
@@ -19,7 +19,6 @@ sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-GXNAS'"
 sed -i "s/hostname='.*'/hostname='OpenWrt-GXNAS'/g" ./package/base-files/files/bin/config_generate
 
 # 修改默认IP
-#sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 sed -i 's#192.168.1.1#192.168.1.11#g' package/base-files/files/bin/config_generate
 
 # 设置旁路由模式
@@ -60,7 +59,8 @@ sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/file
 sed -i 's/OpenWrt/OpenWrt-GXNAS/g' package/base-files/files/bin/config_generate
 
 # 修改版本号
-sed -i "s/DISTRIB_DESCRIPTION=' *LEDE R24.10.24*'/DISTRIB_DESCRIPTION='OpenWrt_x64_旁路由版 by GXNAS build @R$(date +%y.%m.%d) '/" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/DISTRIB_DESCRIPTION='*LEDE R24.10.24*'/DISTRIB_DESCRIPTION='OpenWrt_x64_旁路由版 by GXNAS build @R$(date +%y.%m.%d) '/" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/LEDE R24.10.24/OpenWrt_x64_旁路由版 by GXNAS build @R$(date +%y.%m.%d) /' package/lean/default-settings/files/zzz-default-settings
 
 # 设置ttyd免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
