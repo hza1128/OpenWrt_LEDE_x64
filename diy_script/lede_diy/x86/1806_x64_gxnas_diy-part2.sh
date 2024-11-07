4d4051fd5a120e612611f6920914b60fb11fc88c
3092099 <122336705+3092099@users.noreply.github.com>
2024-11-07 15:32:36 +0800

    Update and rename x64_gxnas_diy-part2.sh to 1806_x64_gxnas_diy-part2.sh

--- diy_script/lede_diy/x86/1806_x64_gxnas_diy-part2.sh
+++ diy_script/lede_diy/x86/1806_x64_gxnas_diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #===============================================
-# Description: DIY script part 2
-# File name: x64_gxnas_diy-part2.sh
+# Description: 1806_x64_gxnas DIY script part 2
+# File name: 1806_x64_gxnas_diy-part2.sh
 # Lisence: MIT
 # By: GXNAS
 #===============================================
