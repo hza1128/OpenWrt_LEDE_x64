121aa8e59fc631a9bd2364863a066b477aded9b5
3092099 <122336705+3092099@users.noreply.github.com>
2024-11-07 09:29:30 +0800

    Update x64_2305_test_diy-part2.sh

--- diy_script/lede_diy/x86/x64_2305_test_diy-part2.sh
+++ diy_script/lede_diy/x86/x64_2305_test_diy-part2.sh
@@ -60,8 +60,7 @@ sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/file
 sed -i 's/OpenWrt/OpenWrt-GXNAS/g' package/base-files/files/bin/config_generate
 
 # 修改版本号
-#sed -i "s/DISTRIB_DESCRIPTION='*LEDE R24.10.24*'/DISTRIB_DESCRIPTION='OpenWrt_x64_测试版 by GXNAS build @R$(date +%y.%m.%d) '/" package/lean/default-settings/files/zzz-default-settings
-sed -i '/R24.10.24/c\OpenWrt_2305_x64_测试版 by GXNAS build @R$(date +%y.%m.%d)' package/lean/default-settings/files/zzz-default-settings
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
 
 # 设置ttyd免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
@@ -205,6 +204,11 @@ sed -i 's|<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank
 
 # 显示增加编译时间
 #sed -i "s/<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%>/<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%> (By @GXNAS build $(TZ=UTC-8 date "+%Y-%m-%d %H:%M"))/g" package/lean/autocore/files/x86/index.htm
+echo "package/base-files/files/etc/openwrt_release："
+cat package/base-files/files/etc/openwrt_release
+echo "修改主机名字后，package/base-files/files/etc/openwrt_release 配置文件内容如下："
+sed -i "s/DISTRIB_REVISION=.*/DISTRIB_REVISION='OpenWrt_x64_全功能版 by GXNAS build @R$(date +%y.%m.%d)'/g" package/base-files/files/etc/openwrt_release
+cat package/base-files/files/etc/openwrt_release
 
 # 修改概览里时间显示为中文数字
 sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/x86/index.htm
