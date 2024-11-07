73b5c6a01f75953f2bbe04ab4b001c710e0e63a7
3092099 <122336705+3092099@users.noreply.github.com>
2024-11-07 09:03:41 +0800

    Update x64_gxnas_diy-part2.sh

--- diy_script/lede_diy/x86/x64_gxnas_diy-part2.sh
+++ diy_script/lede_diy/x86/x64_gxnas_diy-part2.sh
@@ -60,7 +60,7 @@ sed -i 's/OpenWrt/OpenWrt-GXNAS/g' package/base-files/files/bin/config_generate
 
 # 修改版本号
 #sed -i "s/DISTRIB_DESCRIPTION='*LEDE R24.10.24*'/DISTRIB_DESCRIPTION='OpenWrt_x64_旁路由版 by GXNAS build @R$(date +%y.%m.%d) '/" package/lean/default-settings/files/zzz-default-settings
-sed -i 's/LEDE R24.10.24/OpenWrt_x64_旁路由版 by GXNAS build @R$(date +%y.%m.%d) /' package/lean/default-settings/files/zzz-default-settings
+sed -i '/R24.10.24/c\OpenWrt_x64_旁路由版 by GXNAS build @R$(date +%y.%m.%d)' package/lean/default-settings/files/zzz-default-settings
 
 # 设置ttyd免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
